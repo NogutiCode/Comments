50bbad4893cf4893b96b15c4606cf7c2df0b0b89
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-11-02 10:12:36 +0200

    Add users seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(DefaultUserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(ImageSeeder::class);
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
